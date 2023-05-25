96a46ec1a82b8334744aeef7309911ab1519cffc
Jory Roberts <robertsjory13@gmail.com>
2023-05-25 12:01:16 -0500

    lecture continued walkthrough

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import React from "react"
-import "./App.css"
-import { albumCollection } from "./data"
-import Album from "./components/Album"
+import React from 'react';
+import './App.css';
+import { albumCollection } from './data';
+import AlbumList from './components/AlbumList';
 
 function App() {
-   return (
-      <div className="App">
-         <h1>My Music Album Collection</h1>
-         <div className="albums">
-            <Album album={albumCollection[0]} />
-            <Album album={albumCollection[1]} />
-            <Album album={albumCollection[2]} />
-            <Album album={albumCollection[3]} />
-            <Album album={albumCollection[4]} />
-            <Album album={albumCollection[5]} />
-            <Album album={albumCollection[6]} />
-            <Album album={albumCollection[7]} />
-            <Album album={albumCollection[8]} />
-         </div>
-      </div>
-   )
+	return (
+		<div className='App'>
+			<AlbumList albums={albumCollection} />
+		</div>
+	);
 }
 
-// function Album({ album }) {
-//    const { title, artist, year, rating, coverUrl } = album
-
-//    return (
-//       <div className="album">
-//          <h2>{title}</h2>
-//          <p>{artist}</p>
-//          <p>{year}</p>
-//          <p>Rating: {rating}</p>
-//          <img src={coverUrl} alt="" />
-//       </div>
-//    )
-// }
-
-export default App
+export default App;
